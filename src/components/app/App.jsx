201957daad857021c96007c40d7a4eadc44217ce
Your Name <you@example.com>
2024-12-01 14:02:21 -0800

    feat: create main-page

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,16 +3,8 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AppPaths } from '../../constants';
 import NotFoundPage from '../pages/notFound-page/NotFoundPage';
+import MainPage from '../pages/main-page/MainPage';
 import MainLayout from '../../layouts/main-layouts/MainLayout';
-
-
-const MainPage = () => {
-  return (
-    <div>
-      <h1>hello</h1>
-    </div>
-  );
-}
 
 
 
