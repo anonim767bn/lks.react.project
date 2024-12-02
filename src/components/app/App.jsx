9d8d5ff960f8663bdff73f907a3295b83bdce5e9
alex <example@example.example>
2024-12-02 17:14:16 +0300

    feat: create ServicePage and ApiActions

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,27 +1,23 @@
-
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AppPaths } from '../../constants';
 import NotFoundPage from '../pages/notFound-page/NotFoundPage';
 import MainPage from '../pages/main-page/MainPage';
+import ServicesPage from '../pages/services-page/ServicesPage';
 import MainLayout from '../../layouts/main-layouts/MainLayout';
-
-
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={AppPaths.main} element={
-          <MainLayout>
-            <MainPage />
-          </MainLayout>
-        } />
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<MainPage />} />
+          <Route path={AppPaths.services} element={<ServicesPage />} />
+        </Route>
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
 }
 
-
-export default App
+export default App;
---
+++ src/components/pages/services-page/ServicesPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ServicesPage = () => {
+    return (
+        <div>
+            <h1>Услуги</h1>
+            <p>Здесь будет информация о наших услугах</p>
+        </div>
+    );
+};
+
+export default ServicesPage;
